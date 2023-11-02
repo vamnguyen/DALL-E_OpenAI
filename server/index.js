e2f1dace87efa2978a60fdd6c19163f3760d5348
Nguyễn Viết Anh Minh <anhminh.dev@gmail.com>
2023-11-02 20:27:49 +0700

    update project

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ const startServer = async () => {
   try {
     connectDB(process.env.MONGODB_URL);
     app.listen(8080, () =>
-      console.log("Server has started on port https://localhost:8080")
+      console.log("Server has started on port http://localhost:8080")
     );
   } catch (error) {
     console.log(error);
